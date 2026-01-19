9c369c742a1b04a1fe5cc599c6a9d0380d46e309
Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>
2026-01-19 16:50:46 +0300

    feat(Distro): Manjaro support

--- Core/distro_manager.py
+++ Core/distro_manager.py
@@ -341,6 +341,22 @@ class VoidDistribution(TermuxDistribution):
 		termux_arch = self._map_architecture(arch)
 		return super().supports_architecture(termux_arch)
 
+class ManjaroDistribution(TermuxDistribution):
+	def get_name(self) -> str:
+		return "manjaro"
+
+
+	def _map_architecture(self, arch: str) -> str:
+		"""Map standard architecture to Termux-specific names"""
+		termux_arch_map = {
+			'arm64': 'aarch64'
+		}
+		return termux_arch_map.get(arch, arch)
+
+	def supports_architecture(self, arch: str) -> bool:
+		termux_arch = self._map_architecture(arch)
+		return super().supports_architecture(termux_arch)
+
 class AlpineDistribution(Distribution):
 	"""Alpine Linux distribution"""
 
@@ -830,14 +846,16 @@ class DistributionManager:
 			"ubuntu",
 			"archlinux",
 			"fedora",
-			"void"
+			"void",
+			"manjaro"
 		]
 		self.termux_distros_list = [
 			DebianDistribution,
 			UbuntuDistribution,
 			ArchLinuxDistribution,
 			FedoraDistribution,
-			VoidDistribution
+			VoidDistribution,
+			ManjaroDistribution
 		]
 
 		self.distributions: Dict[str, Distribution] = self._initialize_distributions()
--- main.py
+++ main.py
@@ -92,7 +92,8 @@ class AndroSH:
 							"kali-nethunter",
 							"archlinux",
 							"fedora",
-							"void"
+							"void",
+							"manjaro"
 						]
 
 
